78dfdf123b9b43f69fd93ab726b98b5896d55cfa
noah.y <noah.y@krustuniverse.com>
2021-12-05 19:29:43 +0900

    Added ERC-20 Token(NAH, Noah Token)

---
+++ contracts/NoahToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
+
+contract NoahToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Noah Token", "NAH") {
+        _mint(msg.sender, initialSupply);
+        console.log("Token initialized. initialSupply=%s", initialSupply);
+    }
+}

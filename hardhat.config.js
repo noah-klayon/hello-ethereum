63a452688ffbc526c49e6f932788ae248b5c1955
noah.y <noah.y@krustuniverse.com>
2021-12-05 19:53:41 +0900

    Deploy ERC-20(NAH) to Ropsten network

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,4 +5,10 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/BDrWE7j2nRiizdoKQMasNDO0f2H3AUd_",
+      accounts: ["YOUR_ROPSTEN_PRIVATE_KEY"]
+    }
+  }
 };
